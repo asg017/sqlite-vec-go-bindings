2c65aa0f3d23cb1d5ae72b24be86e76c3a2b1459
Alex Garcia <alexsebastian.garcia@gmail.com>
2024-08-01 10:11:18 +0000

    AUTOMATED v0.1.0

--- cgo/sqlite-vec.h
+++ cgo/sqlite-vec.h
@@ -3,9 +3,9 @@
 
 #include "sqlite3ext.h"
 
-#define SQLITE_VEC_VERSION "v0.1.0-alpha.1"
-#define SQLITE_VEC_DATE "2024-08-01T09:56:13Z+0000"
-#define SQLITE_VEC_SOURCE "fa96def687a9e520ba877e095a64dd19811fdda2"
+#define SQLITE_VEC_VERSION "v0.1.0"
+#define SQLITE_VEC_DATE "2024-08-01T10:09:49Z+0000"
+#define SQLITE_VEC_SOURCE "21c5a14fc71c83f135f5b00c84115139fd12c492"
 
 #ifdef __cplusplus
 extern "C" {
