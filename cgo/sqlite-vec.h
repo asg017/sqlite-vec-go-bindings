beaedd5c52a15aec9cf38d2d8b5626441df96c61
Alex Garcia <alexsebastian.garcia@gmail.com>
2024-09-26 06:13:02 +0000

    AUTOMATED v0.1.3-alpha.3

--- cgo/sqlite-vec.h
+++ cgo/sqlite-vec.h
@@ -17,26 +17,16 @@
   #endif
 #endif
 
-#define SQLITE_VEC_VERSION "v0.1.3-alpha.2"
-#define SQLITE_VEC_DATE "2024-09-16T17:49:22Z+0000"
-#define SQLITE_VEC_SOURCE "330f021216d0fd44d60212aaffab822b314822f2"
+#define SQLITE_VEC_VERSION "v0.1.3-alpha.3"
+#define SQLITE_VEC_DATE "2024-09-26T06:11:36Z+0000"
+#define SQLITE_VEC_SOURCE "5b61ec2a8d8f1081c0601aa545b3ef22a673a3d5"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifdef _WIN32
-__declspec(dllexport)
-#endif
-int sqlite3_vec_init(sqlite3 *db, char **pzErrMsg,
+SQLITE_VEC_API int sqlite3_vec_init(sqlite3 *db, char **pzErrMsg,
                   const sqlite3_api_routines *pApi);
-
-#ifdef _WIN32
-__declspec(dllexport)
-#endif
-int sqlite3_vec_fs_read_init(sqlite3 *db, char **pzErrMsg,
-                          const sqlite3_api_routines *pApi);
-
 
 #ifdef __cplusplus
 }  /* end of the 'extern "C"' block */
