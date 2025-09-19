1f9830787013890dd3a524980c4e42b667c43961
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-09-19 11:08:54 -0700

    cgo: add Load() and deprecate Auto()/Cancel(), using reflect to get raw pointers

--- cgo/lib.go
+++ cgo/lib.go
@@ -7,27 +7,15 @@ package vec
 import "C"
 import (
 	"bytes"
+	"context"
+	"database/sql"
 	"encoding/binary"
+	"fmt"
+	"reflect"
+	"unsafe"
+
+	sqlite3 "github.com/mattn/go-sqlite3"
 )
-
-// Once called, every future new SQLite3 connection created in this process
-// will have the sqlite-vec extension loaded. It will persist until [Cancel] is
-// called.
-//
-// Calls [sqlite3_auto_extension()] under the hood.
-//
-// [sqlite3_auto_extension()]: https://www.sqlite.org/c3ref/auto_extension.html
-func Auto() {
-	C.sqlite3_auto_extension( (*[0]byte) ((C.sqlite3_vec_init)) );
-}
-
-// "Cancels" any previous calls to [Auto]. Any new SQLite3 connections created
-// will not have the sqlite-vec extension loaded.
-//
-// Calls sqlite3_cancel_auto_extension() under the hood.
-func Cancel() {
-	C.sqlite3_cancel_auto_extension( (*[0]byte) (C.sqlite3_vec_init) );
-}
 
 // Serializes a float32 list into a vector BLOB that sqlite-vec accepts.
 func SerializeFloat32(vector []float32) ([]byte, error) {
@@ -39,3 +27,28 @@ func SerializeFloat32(vector []float32) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+// Loads the sqlite-vec extension into the given database connection.
+// The connection must be a *sqlite3.SQLiteConn from github.com/mattn/go-sqlite3.
+// Beware! this unsafe and reflection to access the underlying C pointer.
+func Load(db *sql.DB) error {
+	conn, err := db.Conn(context.Background())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	return conn.Raw(func(driverConn interface{}) error {
+		sc, ok := driverConn.(*sqlite3.SQLiteConn)
+		if !ok {
+			return fmt.Errorf("not sqlite3 conn")
+		}
+		dbVal := reflect.ValueOf(sc).Elem().FieldByName("db")
+		dbPtr := unsafe.Pointer(dbVal.Pointer())
+		raw := (*C.sqlite3)(dbPtr)
+		rc := C.sqlite3_vec_init(raw, nil, nil)
+		if rc != C.SQLITE_OK {
+			return fmt.Errorf("sqlite3_vec_init failed: %d", int32(rc))
+		}
+
+		return nil
+	})
+}
